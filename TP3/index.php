fb061f6ec6c8f2d9ffe7a15d0e5c24f3f2f8b4e4
ZhiweiPEI <zhiwei.pei@outlook.com>
2021-03-17 22:31:20 +0100

    x

--- TP3/index.php
+++ TP3/index.php
@@ -1,8 +1,22 @@
-<form id="style_form" action="index.php" method="GET">
- <select name="css">
- <option value="style1">style1</option>
- <option value="style2">style2</option>
- </select>
- <input type="submit" value="Appliquer" />
-</form>
-
+<?php
+setcookie("usercss", "style1", time() + 3600);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+            $stylesheetname = "'" . $_COOKIE["usercss"] . ".css'";
+            echo "<link rel='stylesheet' href=" . $stylesheetname .">";
+        ?>
+        <title>TP3-1.3</title>
+    </head>
+    <body>
+        <form id="style_form" action="change.php" method="POST">
+            <select name="css">
+                <option value="style1">style1</option>
+                <option value="style2">style2</option>
+            </select>
+            <input type="submit" value="Appliquer" />
+        </form>
+    </body>
+</html>
